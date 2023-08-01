15a5bca65b2a29113bf93c8a83ce74b89b3a1ce8
Rok Garbas <rok@floxdev.com>
2023-08-01 23:48:54 +0200

    strip mkdocs annotation comments

--- src/tesh/extract.py
+++ src/tesh/extract.py
@@ -173,6 +173,8 @@ def extract_blocks(session: ShellSession, verbose: bool) -> None:
     blocks = []
 
     for line in session.lines:
+        # remove mkdocs style of code anotations
+        line = re.sub(r"\# \(.*\)!$|\# \(.*\)$", "", line).strip()
         if m := prompt.match(line):
             if new_block.command:
                 blocks.append(new_block)
